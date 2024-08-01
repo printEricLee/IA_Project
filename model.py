4af3f89a1fb9f8325938538f548cec7d82ce7fcb
printEricLee <xyxz55124019@gmail.com>
2024-08-01 12:34:30 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,8 +1,25 @@
 import cv2
 from ultralytics import YOLO
+import touch
+import os
+
+# data type
+accept = ''
+unaccepted = ''
 
 # import data(jpg/mp3)
 data = []
-label = []
+labels = []
 
-print(cv2.__version__)
+model = touch.load("model_Separate-materials.py")
+
+def load_images(folder, label):
+    for filename in os.listdir(folder):
+        img_path = os.path.join(folder, filename)
+        img = cv2.imread(img_path)
+        if img is not None:
+            img = cv2.resize(img, (64,64))
+            data.append(img)
+            labels.append(label)
+
+load_images(<data name> , <number of data>)
