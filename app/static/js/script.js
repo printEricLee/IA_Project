e8c9aad15c628bb036976f0323fcb79e7c5f30b3
printericlee <xyxz55124019@gmail.com>
2024-12-10 00:40:42 +0800

    Update script.js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -17,30 +17,30 @@ function hide_word() {
 //##############################################################################################
 // live detecting
 
-function updateDetectionStatus(detectedItems) {
-    const items = document.querySelectorAll('#detection-list li');
+// function updateDetectionStatus(detectedItems) {
+//     const items = document.querySelectorAll('#detection-list li');
 
-    items.forEach(item => {
-        const itemName = item.getAttribute('data-item');
-        if (detectedItems.includes(itemName)) {
-            item.classList.remove('not-detected');
-            item.classList.add('detected');
-        } else {
-            item.classList.remove('detected');
-            item.classList.add('not-detected');
-        }
-    });
-}
+//     items.forEach(item => {
+//         const itemName = item.getAttribute('data-item');
+//         if (detectedItems.includes(itemName)) {
+//             item.classList.remove('not-detected');
+//             item.classList.add('detected');
+//         } else {
+//             item.classList.remove('detected');
+//             item.classList.add('not-detected');
+//         }
+//     });
+// }
 
-setInterval(() => {
-    fetch('/live-detect')
-        .then(response => response.json())
-        .then(data => {
-            const detectedItems = data.detected_items;
-            updateDetectionStatus(detectedItems);
-        })
-        .catch(error => console.error('Error fetching detection data:', error));
-}, 5000);
+// setInterval(() => {
+//     fetch('/live-detect')
+//         .then(response => response.json())
+//         .then(data => {
+//             const detectedItems = data.detected_items;
+//             updateDetectionStatus(detectedItems);
+//         })
+//         .catch(error => console.error('Error fetching detection data:', error));
+// }, 5000);
 
 //##############################################################################################
 // image detecting
