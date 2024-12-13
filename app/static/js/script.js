ef90df99592b7b873973bb0f8760985ff193d81d
printericlee <xyxz55124019@gmail.com>
2024-12-13 15:15:35 +0800

    Update script.js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -19,36 +19,4 @@ function show_word() {
     loadingShowMessage.style.display = 'flex';
 }
 
-//##############################################################################################
-// live detecting
-
-// function updateDetectionStatus(detectedItems) {
-//     const items = document.querySelectorAll('#detection-list li');
-
-//     items.forEach(item => {
-//         const itemName = item.getAttribute('data-item');
-//         if (detectedItems.includes(itemName)) {
-//             item.classList.remove('not-detected');
-//             item.classList.add('detected');
-//         } else {
-//             item.classList.remove('detected');
-//             item.classList.add('not-detected');
-//         }
-//     });
-// }
-
-// setInterval(() => {
-//     fetch('/live-detect')
-//         .then(response => response.json())
-//         .then(data => {
-//             const detectedItems = data.detected_items;
-//             updateDetectionStatus(detectedItems);
-//         })
-//         .catch(error => console.error('Error fetching detection data:', error));
-// }, 5000);
-
-//##############################################################################################
-// image detecting
-
-//##############################################################################################
 
